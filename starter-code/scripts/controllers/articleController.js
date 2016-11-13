f2ed9cc3c5df778998a3372bdd3175af800ec41c
Scott Schmidt <sjschmidt44@gmail.com>
2016-11-13 10:11:39 -0800

    base line

--- starter-code/scripts/controllers/articleController.js
+++
@@ -1,9 +0,0 @@
-(function(module) {
-  var articleController = {};
-
-  articleController.reveal = function() {
-    /* TODO: Use your DOM skills to reveal only the articles section! */
-  };
-
-  module.articleController = articleController;
-})(window);
